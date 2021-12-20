572ac0a267184955e10a145505d0b7f6908943a7
Nazilyauser.name <wkwad@mail.ru>
2021-12-21 03:11:24 +0500

    dz5

--- src/test/java/com/geekbrains/tests/GetProductsPositiveTests.java
+++ src/test/java/com/geekbrains/tests/GetProductsPositiveTests.java
@@ -7,6 +7,7 @@ import com.geekbrains.utils.PrettyLogger;
 import okhttp3.ResponseBody;
 import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import retrofit2.Response;
@@ -18,7 +19,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
 public class GetProductsPositiveTests extends BaseTest {
-
+    @Disabled
     @Test
     void getProducts() throws IOException {
         Response<ArrayList<Product>> response = productService
